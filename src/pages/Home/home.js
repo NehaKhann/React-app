278e0f1272eae4a4c5f598b9920554a4c209bb9e
NehaKhann <n.nehakhan333@gmail.com>
2022-09-26 10:53:27 +0500

    login and home screen

---
+++ src/pages/Home/home.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+const Home = () => {
+    const [authenticated, setauthenticated] = useState(null);
+    const [pageLoad, setpageLoad] = useState(true)
+
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("authenticated");
+        if (loggedInUser) {
+            setauthenticated(loggedInUser);
+        }
+        setpageLoad(false)
+    }, []);
+
+    if (pageLoad == false && !authenticated) {
+        // Redirect
+        return <Navigate replace to="/login" />;
+    } else {
+        return (
+            <div>
+                <p>Welcome to your Home Screen</p>
+            </div>
+        );
+    }
+};
+export default Home;
